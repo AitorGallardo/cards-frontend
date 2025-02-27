d7480872c5069a95a0a2a60b511ecc4eb5bec56f
AitorGallardo <38296940+AitorGallardo@users.noreply.github.com>
2025-02-27 15:56:05 +1100

    first impleementation

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from './App.jsx'
-import Grid from './pages/Grid.jsx'
-import GridV2 from './pages/GridV2.jsx'
-import GridFinal from './pages/Grid-final.jsx'
-import Layout from './components/Layout.jsx'
+import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<App />} />
-          <Route path="grid" element={<Grid />} />
-          <Route path="grid-v2" element={<GridV2 />} />
-          <Route path="grid-final" element={<GridFinal />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
 )
